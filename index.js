77829266a05280b52dcc802f53e5bf00a144d96b
Arpit Sharma <33283252+arpitsharmagit@users.noreply.github.com>
2018-01-07 14:55:54 +0530

    api integration

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 import mailer from './lib/mailer';
 import koinexApi from './lib/koinex.api';
+import alertServer from './api/server';
 import sendNotification from './lib/message.api';
+import * as utils from './lib/utils';
 
-function sleep(seconds) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        }, seconds * 1000);
-    })
-};
 async function main() {
-    let currenPrice = await koinexApi.getCurrentPrice();
-    console.log(`Ripple: ${currenPrice.XRP}`)
-    let count = 0;
-    while (count < 10) {
-        count++;    
-        await sleep(5);
-        currenPrice = await koinexApi.getCurrentPrice();      
-        console.log(`Ripple: ${currenPrice.XRP}`)  
-        const title = 'Alerto: Price Alert',
-            body = `Ripple: ${currenPrice.XRP}`, registerId = '';
-        sendNotification(registerId, title, body);
-    } 
-    console.log("done!");
-
-
-    // if (currenPrice) {
-    //     let mailOptions = {
-    //         from: '"Arpit Mailer" <arpitsharma401628@gmail.com>', // sender address
-    //         to: 'arpitsharma1282@gmail.com', // list of receivers
-    //         subject: 'Hello Dude ✔', // Subject line
-    //         text: `BitCoin => ${currenPrice.BTC}\n Ripple => ${currenPrice.XRP}\n Ether => ${currenPrice.ETH}\n BitCash => ${currenPrice.BCH} \nLiteCoin => ${currenPrice.LTC}`, // plain text body
-    //         html: `BitCoin => ${currenPrice.BTC}<br> Ripple => ${currenPrice.XRP}<br> Ether => ${currenPrice.ETH}<br> BitCash => ${currenPrice.BCH} <br>LiteCoin => ${currenPrice.LTC}` // html body
-    //     };
-    //     mailer.sendMail(mailOptions);
-    // }
+    alertServer();
+    // let currenPrice = await koinexApi.getCurrentPrice();
+    // let title = 'Alerto: Price Alert',
+    //     body = `Ripple: ${currenPrice.XRP}`, registerId = '';
+    // console.log(`Ripple: ${currenPrice.XRP}`)
+    // sendNotification(registerId, title, body);    
+    // console.log("done!");
 }
 main();
