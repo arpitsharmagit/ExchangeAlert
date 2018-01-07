e6d53cec3a4c01b79dd0e523f4e7b2a3a52bbd17
Arpit Sharma <33283252+arpitsharmagit@users.noreply.github.com>
2018-01-07 14:57:44 +0530

    api commit

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ import sendNotification from './lib/message.api';
 import * as utils from './lib/utils';
 
 async function main() {
+    setTimeout(() => {
+        let currenPrice = await koinexApi.getCurrentPrice();
+        let title = 'Alerto: Price Alert',
+            body = `Ripple: ${currenPrice.XRP}`, registerId = '';
+        console.log(`Ripple: ${currenPrice.XRP}`)
+        sendNotification(title, body);
+        console.log("done!");
+    }, (2000));
     alertServer();
-    // let currenPrice = await koinexApi.getCurrentPrice();
-    // let title = 'Alerto: Price Alert',
-    //     body = `Ripple: ${currenPrice.XRP}`, registerId = '';
-    // console.log(`Ripple: ${currenPrice.XRP}`)
-    // sendNotification(registerId, title, body);    
-    // console.log("done!");
 }
 main();
